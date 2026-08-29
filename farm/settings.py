a38095a9948e0c18895d99006394f4d214f31b73
sreedarshs2002 <sreedarshs2002@gmail.com>
2026-08-29 14:26:34 +0530

    Fix allowed hosts for Render

--- farm/settings.py
+++ farm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['192.168.29.160', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
